0ae7c1c4b14e344b6e9b1fc206e2d97192e199a2
kabbo500 <kabbokazi3779@gmail.com>
2022-05-25 12:37:13 +0800

    create admin

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -15,10 +20,10 @@ const Dashboard = () => {
                 <label for="my-drawer-2" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to="/dashboard">My Orders</Link></li>
-                    <li><Link to="/dashboard/addReview">Add Reviews</Link></li>
+                    {!admin && <li><Link to="/dashboard">My Orders</Link></li>}
+                    {!admin && <li><Link to="/dashboard/addReview">Add Reviews</Link></li>}
                     <li><Link to="/dashboard/myProfile">My Profile</Link></li>
-                    <li><Link to="/dashboard/makeAdmin">Make Admin</Link></li>
+                    {admin && <li><Link to="/dashboard/makeAdmin">Make Admin</Link></li>}
                 </ul>
 
             </div>
--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
+import AdminRow from './AdminRow';
 
 const MakeAdmin = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1 className='text-2xl text-primary'>Admin panel</h1>
+            <h1 className='text-2xl text-primary'>Admin panel: {users.length}</h1>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users.map(user => <AdminRow key={user._id} user={user}
+                                refetch={refetch}
+                            >
+                            </AdminRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
