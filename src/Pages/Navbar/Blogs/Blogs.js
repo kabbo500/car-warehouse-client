508c5d3e2e10a57dead9fc70811e7c0b1cf03b37
kabbo500 <kabbokazi3779@gmail.com>
2022-05-26 14:46:51 +0800

    final touch

--- src/Pages/Navbar/Blogs/Blogs.js
+++ src/Pages/Navbar/Blogs/Blogs.js
@@ -45,6 +45,22 @@ const Blogs = () => {
 
                     Prototypal inheritance is a language feature that helps in that.When we read a property from object, and it's missing, JavaScript automatically takes it from the prototype. In programming, this is called “prototypal inheritance”. And soon we'll study many examples of such inheritance, as well as cooler language features built upon it.</p>
             </article>
+            <article>
+                <h3 className='text-primary mt-4 text-3xl'>4) What is a unit test? Why should write unit tests?</h3>
+                <p> <b>Ans:</b>
+                    Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.
+                    The main objective of unit testing is to isolate written code to test and determine if it works as intended.
+                    A unit test typically comprises of three stages: plan, cases and scripting and the unit test itself. In the first step, the unit test is prepared and reviewed. The next step is for the test cases and scripts to be made, then the code is tested.
+
+                    Test-driven development requires that developers first write failing unit tests. Then they write code and refactor the application until the test passes. TDD typically results in an explicit and predictable code base.
+                    The earlier a problem is identified, the fewer compound errors occur.
+                    Costs of fixing a problem early can quickly outweigh the cost of fixing it later.
+                    Debugging processes are made easier.
+                    Developers can quickly make changes to the code base.
+                    Developers can also re-use code, migrating it to new projects.
+
+                </p>
+            </article>
 
         </div>
     );
