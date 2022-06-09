dabe8323912369cbcccbce55bdf25bb6803c0000
kabbo500 <kabbokazi3779@gmail.com>
2022-06-09 19:03:22 +0800

    change text color

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,8 +9,8 @@ const Banner = () => {
             <div style={{ backgroundImage: `url(${url2})` }} class="hero min-h-screen bg-base-200">
                 <div class="hero-content text-center">
                     <div class="max-w-md">
-                        <h1 class="text-6xl font-bold text-orange-400">K. K. Electronic And Co..</h1>
-                        <p class="py-6 text-white text-2xl">We provide the  quality product with Accessible pice and Best after sales service</p>
+                        <h1 class="text-6xl font-bold text-purple-500">K. K. Electronic And Co..</h1>
+                        <p class="py-6 text-2xl">We provide the  quality product with Accessible pice and Best after sales service</p>
 
                     </div>
                 </div>
